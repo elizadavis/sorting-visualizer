e86eac6cd6aae656de40ec5e470d0aa4e57f015f
David Ko <david.holy.ko@gmail.com>
2020-01-16 20:23:33 -0500

    feat(algorithms): implement insertion sort

--- src/Logic/algorithms.js
+++ src/Logic/algorithms.js
@@ -1,56 +1,61 @@
 const bubbleSort = unsortedArray => {
-  let isSorted = false
-  let lastUnsorted = unsortedArray.length - 1
-  const phases = []
+  let isSorted = false;
+  let lastUnsorted = unsortedArray.length - 1;
+  const phases = [];
   while (!isSorted) {
-    isSorted = true
+    isSorted = true;
     for (let i = 0; i < lastUnsorted; i++) {
       if (unsortedArray[i] > unsortedArray[i + 1]) {
-        swap(i, i + 1, unsortedArray)
-        isSorted = false
+        swap(i, i + 1, unsortedArray);
+        isSorted = false;
       }
-      phases.push([...unsortedArray])
+      phases.push([...unsortedArray]);
     }
-    lastUnsorted--
+    lastUnsorted--;
   }
-  return phases
-}
+  return phases;
+};
 
 const mergeSort = unsortedArray => {
   // @todo
 
-  return [unsortedArray]
-}
+  return [unsortedArray];
+};
 
 const quickSort = unsortedArray => {
   // @todo
 
-  return [unsortedArray]
-}
+  return [unsortedArray];
+};
 
 const heapSort = unsortedArray => {
   // @todo
 
-  return [unsortedArray]
-}
+  return [unsortedArray];
+};
 
 const insertionSort = unsortedArray => {
-  for (let i = 1; i <= unsortedArray.length - 1; i++) {
-    let value = unsortedArray[i]
-    let hole = i
-    while (hole > 0 && unsortedArray[hole - 1] > value) {
-      unsortedArray[hole] = unsortedArray[hole - 1]
-      hole = hole - 1
+  const phases = [];
+  let indexStart = 1;
+  while (indexStart < unsortedArray.length) {
+    let indexEnd = indexStart;
+    while (
+      indexEnd > 0 &&
+      unsortedArray[indexEnd] < unsortedArray[indexEnd - 1]
+    ) {
+      swap(indexEnd, indexEnd - 1, unsortedArray);
+      indexEnd = indexEnd - 1;
+      phases.push([...unsortedArray]);
     }
-    unsortedArray[hole] = value
+    indexStart++;
   }
-  return [unsortedArray]
-}
+  return phases;
+};
 
 function swap(indexA, indexB, array) {
-  let temp = array[indexA]
-  array[indexA] = array[indexB]
-  array[indexB] = temp
+  let temp = array[indexA];
+  array[indexA] = array[indexB];
+  array[indexB] = temp;
 }
 
 export const sorts = {
@@ -58,5 +63,5 @@ export const sorts = {
   MERGE_SORT: mergeSort,
   QUICK_SORT: quickSort,
   HEAP_SORT: heapSort,
-  INSERTION_SORT: insertionSort
-}
+  INSERTION_SORT: insertionSort,
+};
