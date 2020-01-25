467e3e504904fac6ee974e108f50d4a8254807cd
David Ko <david.holy.ko@gmail.com>
2020-01-25 18:58:33 -0500

    feat(footer): add footer

--- src/Components/Main/Footer.js
+++ src/Components/Main/Footer.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="footer">
+      <div>Want to contribute to this project?</div>
+      <div>
+        Check out the repository{' '}
+        <a
+          href="https://github.com/dkjk/sorting-visualizer"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          here
+        </a>
+      </div>
+    </footer>
+  );
 };
