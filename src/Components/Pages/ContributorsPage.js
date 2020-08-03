4569be1f23fd83a94f3c0728a80297b4bd3187d5
elizadavis <eliza272@aim.com>
2020-08-03 14:58:38 -0400

    refactor(contributors): modify contributors page layout

--- src/Components/Pages/ContributorsPage.js
+++ src/Components/Pages/ContributorsPage.js
@@ -5,11 +5,11 @@ import { _ } from 'Utils';
 const Card = props => {
   const { title, name, github, about, website } = props;
   return (
-    <div className="card">
-      <div className="card-name">{name}</div>
-      <div className="card-image">
-        <img src="https://i.imgur.com/IyxFgGe.png" alt=""></img>
+    <div className="card border-0">
+      <div className="card-image my-2">
+        <img src="https://i.imgur.com/IyxFgGe.png" alt="" className="w-25 rounded-circle"></img>
       </div>
+      <div className="card-name"><h4>{name}</h4></div>
       <div className="card-title">{title}</div>
       <div className="card-github">{github}</div>
       <div className="card-about">{about}</div>
@@ -22,11 +22,18 @@ export const ContributorsPage = () => {
   return (
     <div className="contributors-page">
       <div className="contributors-header">
-        <h1>Meet Our Contributors</h1>
+        <h1 className="mt-2">Meet Our Contributors</h1>
+        <p className="col-5 mx-auto">Thank you for using Sorting Visualizer. It takes a village of three so far and these
+          are our members. If you would like to contact us directly, our information is below. We hope
+          to add new features soon.
+        </p>
       </div>
-      {_.map(contributors, (props, index) => (
-        <Card key={index} {...props} />
-      ))}
+      <hr className="w-75" />
+      <div className="card-group">
+        {_.map(contributors, (props, index) => (
+          <Card key={index} {...props} />
+        ))}
+      </div>
     </div>
   );
 };
